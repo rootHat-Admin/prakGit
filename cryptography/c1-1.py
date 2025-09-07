00a22eea1cf4625bdf339307a2c2174db5f3e51f
PopAdmin <maksim.biryukov.2008@gmail.com>
2025-09-07 22:28:01 +0500

    Py Cripto

---
+++ cryptography/c1-1.py
@@ -0,0 +1,6 @@
+# Decimal number
+num = 1010
+
+print("Binary:", bin(num))
+print("Octal:", oct(num))
+print("Hexadecimal:", hex(num))
---
+++ cryptography/c1-2.py
@@ -0,0 +1,4 @@
+num = "12"
+
+print(int(num, 2))
+print(int(num, 8))
