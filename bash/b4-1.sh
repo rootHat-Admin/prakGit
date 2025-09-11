df50eef227563b2e9f07a2e1db7d724e03c577a6
SysAdmin-v <maksim.biryukov.2008@gmail.com>
2025-09-11 15:07:36 +0500

    b4-1..3.sh

---
+++ bash/b4-1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# simple system info script 
+
+# vaeibled
+USERNAME=$USER
+HOSTNAME=$HOSTNAME
+DATE=$(date)
+
+echo "User: $USERNAME"
+echo "Host: $HOSTNAME"
+echo "Date: $DATE"
---
+++ bash/b4-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script with arguments
+
+# $1 = one orgumend, $2 = two orgument
+
+IP=$1
+PORT=$2
+
+echo "Target IP: $IP"
+echo "Target Port: $PORT"
