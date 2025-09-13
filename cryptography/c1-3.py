06e3a52671127b43edac2000901542eb2e676ffd
PopAdmin <maksim.biryukov.2008@gmail.com>
2025-09-13 20:45:13 +0500

    cripto

---
+++ cryptography/c1-3.py
@@ -0,0 +1,5 @@
+binary = "1101" # Binary
+print(int(binary, 2)) # Decimal
+
+num2 = 116 # Decimal
+print(bin(num2)) # Binary
---
+++ cryptography/c1-4.py
@@ -0,0 +1,15 @@
+n = 116
+hex_value = hex(n)
+print(hex_value)
+
+myData = 9022008
+hex_myD = hex(myData)
+print(hex_myD)
+
+den_Data = 2042009
+hex_den_Data = hex(den_Data)
+print(hex_den_Data)
+
+rand_Data = 11111111
+hex_rand_Data = hex(rand_Data)
+print(hex_rand_Data)
