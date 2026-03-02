5c550a84f9596e585a30d9c889306c9db08b3137
Fedora_Linux <maksim.biryukov.2008@gmail.com>
2026-03-02 22:18:08 +0500

    functions

---
+++ js/functions/f1.js
@@ -0,0 +1,31 @@
+// Ex-1
+function sayHello() {
+    console.log("Hello!");
+}
+
+// Ex-2
+function green(name) {
+    console.log("Hello " + name);
+}
+
+// Ex-3
+function add(a, b) {
+    return a + b;
+}
+let result = add(5, 3);
+// console.log(result); 
+
+// Ex-4
+function multiply(x, y) {
+    return x * y;
+}
+// console.log(multiply(4, 5))
+
+// Ex-5
+const subtract = (a, b) => {
+    return a - b;
+};
+console.log(subtract(10, 3));
+
+// ---
+// green("Anna");
