0d3a2b6ad4faecf3c4111d945c1dda015c15981e
Fedora_Linux <maksim.biryukov.2008@gmail.com>
2026-03-02 22:15:52 +0500

    data-types AND functions

---
+++ js/data-types/dt1.js
@@ -0,0 +1,23 @@
+// Number
+let age = 25;
+let price = 10.5;
+
+// String
+let name = "Alex";
+let city = "London";
+
+// Boolean
+let isStudent = true;
+let isStudent_f = false;
+
+// Array
+let colors = ["red", "green", "blue"];
+
+// Object
+let person = {
+    name: "John",
+    age: 30
+};
+// ---
+
+console.log(person.age); 
