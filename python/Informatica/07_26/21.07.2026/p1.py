5028c180975494a13294dbbd570c4d742fab22ad
Skynet <skynet@github.com>
2026-07-21 09:38:41 +0500

    factorial 21.07

---
+++ python/Informatica/07_26/21.07.2026/p1.py
@@ -0,0 +1,11 @@
+print(f" {"Topic: Function - factorial "} ".center(90, '-'))
+
+def factorial(n):
+    result = 1
+
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
+
+print(factorial(5))
