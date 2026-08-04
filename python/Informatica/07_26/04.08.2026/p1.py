245042671ec3e4d0dc65426b5d0647f0d1714816
Skynet <skynet@github.com>
2026-08-04 14:05:31 +0500

    04.08.2026

---
+++ python/Informatica/07_26/04.08.2026/p1.py
@@ -0,0 +1,8 @@
+print(f" {"Topic: Python_Graphs - Adjacency List"} ".center(90, '-'))
+
+graph = {
+    "A" : ["B", "C"],
+    "B" : ["A", "D"],
+    "C" : ["A", "D"],
+    "D" : ["B", "C"]
+}
---
+++ python/Informatica/07_26/04.08.2026/p2.py
@@ -0,0 +1,8 @@
+print(f" {"Topic: Python_Graphs - Adjacency Matrix"} ".center(90, '-'))
+
+graph = [
+    [0, 1, 1, 0],
+    [1, 0, 0, 1],
+    [1, 0, 0, 1],
+    [0, 1, 1, 0]
+]
